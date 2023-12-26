a24d4024af47f6f861566a14505166318ce7f448
ByungheeMoon <153619694+ByungheeMoon@users.noreply.github.com>
2023-12-27 02:14:58 +0900

    Update MBH_UIManager.cs

--- Project/Assets/Scripts/MBH_UIManager.cs
+++ Project/Assets/Scripts/MBH_UIManager.cs
@@ -1,44 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
+
+public class monster 
+{
+    public string name;
+    public int curHp;
+    public int maxHp;
+    public int minAttack;
+    public int maxAttack;
+
+    public monster(string monsterName, int monsterMaxHp, int monsterCurHp, int monsterMaxAttack, int monsterMinAttack)
+    {
+       name = monsterName;
+       maxHp = monsterMaxHp;
+       curHp = monsterCurHp;
+       maxAttack = monsterMinAttack;
+       minAttack = monsterMinAttack;
+
+    }
+}
+
+public class card
+{
+    public string name;
+    public int attack;
+    public int hp;
+    public card(string cardName, int cardAttack, int cardHp)
+    {
+        name = cardName;
+        attack = cardAttack;
+        hp = cardHp;
+
+    }
+}
+
+
+
 
 
 public class MBH_UIManager : MonoBehaviour
 {
     public int playerMaxHp;
     public int playerCurHp;
-
-    public int mon1MaxHp;
-    public int mon1CurHp;
-    public int mon1MinAttack;
-    public int mon1MaxAttack;
-    public int mon1Damage;
-
-    public int mon2MaxHp;
-    public int mon2CurHp;
-    public int mon2MinAttack;
-    public int mon2MaxAttack;
-    public int mon2Damage;
-
-
-    public int mon3MaxHp;
-    public int mon3CurHp;
-    public int mon3MinAttack;
-    public int mon3MaxAttack;
-    public int mon3Damage;
-
-
     public int cardValue;
     public int cardMaxValue = 5;
-
-
+    int monsterExist;
+    int monsterSpawn;
     bool turn;
 
-    bool monsterExist; // 몬스터 존재여부 판별  
+     // 몬스터 존재여부 판별  
+
+   
+
 
     public List<string> monsters = new List<string>();
     
@@ -50,11 +69,20 @@ public class MBH_UIManager : MonoBehaviour
 
     public void Start()
     {
-        monsters.Add("고블린");
-        monsters.Add("오크");
-        monsters.Add("트롤");
-        monsters.Add("좀비");
-        monsters.Add("슬라임");
+        monster goblin= new monster("고블린", 100, 100, 12, 10);
+        monsters.Add("goblin");
+
+        monster orc = new monster("오크", 120, 120, 14, 9);
+        monsters.Add("orc");
+
+        monster troll = new monster("트롤", 110, 110, 10, 10);
+        monsters.Add("troll");
+        
+        monster zombie = new monster("좀비", 80, 80, 7, 7);
+        monsters.Add("zombie");
+
+        monster slime = new monster("슬라임", 60, 60, 6, 6);
+        monsters.Add("slime");
 
         cards.Add("공격1");
         cards.Add("공격2");
@@ -83,11 +111,14 @@ public class MBH_UIManager : MonoBehaviour
         
     }
 
+  
+
+
     void Summon()
     {
-        if (!monsterExist)
-        { 
-          
+        if (monsterExist==0)
+        {
+            monsterSpawn = Random.Range(1, 4);   
         }
     }
 
@@ -103,14 +134,14 @@ public class MBH_UIManager : MonoBehaviour
         else if (turn)
         {
             int monAttack = Random.Range(1, 3);
-            playerCurHp = playerCurHp - mon1Damage;
+            
             turn = false;
 
         }
 
         else if (turn)
         {
-            playerCurHp = playerCurHp - mon1Damage - mon2Damage;
+            
             turn = false;
         }
 
@@ -132,7 +163,6 @@ public class MBH_UIManager : MonoBehaviour
 
 
 }
-
 
 
 // TCG 구조설계 
