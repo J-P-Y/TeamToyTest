07e76261cecbc073428ad6a7cb4098b39de34e53
조동형 <153613537+J-P-Y@users.noreply.github.com>
2023-12-27 15:09:57 +0900

    1227update
    11

--- Project/Assets/Scripts/JoCardManager.cs
+++ Project/Assets/Scripts/JoCardManager.cs
@@ -7,10 +7,89 @@ using UnityEngine.UI;
 
 public class JoCardManager : MonoBehaviour
 {
-    public List<JoMonster> monsters = new List<JoMonster>();
+    List<JoMonster> monsters=new List<JoMonster>(); // 몬스터리스트
 
-    private void Start()
+    List<JoMonster> monsterrand=new List<JoMonster>(); // 소환할 리스트 
+
+    public TextMeshProUGUI[] mnameText;
+    public TextMeshProUGUI[] matkText;
+    public TextMeshProUGUI[] mhpText;
+    public Image[] MonImg;
+
+    public Sprite[] Monspr;
+
+    int hp=100;
+    public TextMeshProUGUI userHp;
+
+    void Start()
     {
         
+
+        AddMonsters("아", 120, 120);
+        AddMonsters("아2", 120, 105);
+        AddMonsters("아3", 120, 105);
+        
+        RandSpawn(3);
+
+
+        Debug.Log(monsterrand[0].monname);
+        Debug.Log(monsterrand[1].monname);
+        Debug.Log(monsterrand[2].monname);
+
+        Attack(0);
+  
     }
+    void RandSpawn(int _cnt)
+    {
+        for (int i = 0; i < _cnt; i++)
+        {
+            JoMonster newMonster2 = monsters[Random.Range(0, monsters.Count)];
+            monsterrand.Add(newMonster2);
+            mnameText[i].text = monsterrand[i].monname;
+            matkText[i].text = ""+monsterrand[i].atk;
+            mhpText[i].text = monsterrand[i].currentHp + "/" + monsterrand[i].maxHp;
+        }
+    }
+    void AddMonsters(string _name,int _atk,int _hp)
+    {
+        JoMonster newMonster = new JoMonster(_name, _atk, _hp);
+        monsters.Add(newMonster);
+    }
+
+    public void Attack(int i)
+    {
+        monsterrand[i].currentHp -= 10;
+        if (monsterrand[i].currentHp<0)
+        {
+            monsterrand[i].Die();
+        }
+        UIdate();
+    }
+
+
+  
+    public void MonsterAtk()
+    {
+        for (int i = 0; i < monsterrand.Count; i++) {
+            if (monsterrand[i].die == true)
+            {
+                hp -= monsterrand[i].atk;
+            }
+        }
+        userHp.text = "" + hp;
+    }
+
+    public void UIdate()
+    {
+        for (int i = 0; i < monsterrand.Count; i++)
+        {
+            mnameText[i].text = monsterrand[i].monname;
+            matkText[i].text = "" + monsterrand[i].atk;
+            mhpText[i].text = monsterrand[i].currentHp + "/" + monsterrand[i].maxHp;
+        }
+    }
+
+    
+
+    
 }
--- Project/Assets/Scripts/JoMonster.cs
+++ Project/Assets/Scripts/JoMonster.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
-public class JoMonster:MonoBehaviour
+using UnityEngine.UI;
+public class JoMonster
 {
-    public string name;
+    //값을 받는 것
+    public string monname;
     public int atk;
     public int maxHp;
     public int currentHp;
+    public Sprite monSpr;
+    public bool die;
 
-    public JoMonster(string _name,int _atk,int _hp)
+    //연동
+    public TextMeshProUGUI nametext;
+    public TextMeshProUGUI atktext;
+    public TextMeshProUGUI hptext;
+    public Image monImg;
+
+    public JoMonster(string _name,int _atk,int _hp) // 생성자
     {
-        name = _name;
+        monname = _name;
         atk = _atk;
         maxHp = _hp;
         currentHp = _hp;
+        die = true;
+    }
+
+    public void Die()
+    {
+        die = false;
     }
 }
